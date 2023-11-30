4a634e5d71f584ce67a299a4c83c2c2b87845d08
KARIMA <karima.jamazi@myyahoo.com>
2023-11-30 16:52:29 +0100

    continuation

--- views/details.php
+++ views/details.php
@@ -1,7 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+</html>
 <?php
 session_start();
 require_once $_SERVER['DOCUMENT_ROOT'] . '/projet_final/function/database.php';
 require_once "../model/vetementModel.php";
+
+
 
 // Vérifier si l'ID du produit est présent dans l'URL
 
@@ -12,17 +25,39 @@ if (isset($_GET['id'])) {
     // Appeler la méthode pour récupérer les détails du produit
     $productDetails = Vetement::getProductDetails($product_id);
 
-    
+
 
     // Vérifier si les détails du produit ont été récupérés avec succès
     if ($productDetails) {
         // Afficher les détails du produit
-        echo '<div>';
+        echo '<div class="block_detail">';
         echo '<img src="./assets/img/' . $productDetails['image'] . '" alt="" width="600" height="500">';
         echo '<h4>' . $productDetails['description'] . '</h4>';
-        echo '<p>' . $productDetails['descriptionDetaille'] . '</p>';
-        echo '<p>Prix: ' . $productDetails['prix'] .'€' .'</p>';
-     
+        $description = str_replace('S Buste', '<span class="couleur1">S Buste</span>', $productDetails['descriptionDetaille']);
+
+        $description = str_replace('Longueur', '<span class="couleur1">Longueur</span>', $description);
+        $description = str_replace('Épaule', '<span class="couleur1">Épaule</span>', $description);
+        $description = str_replace('M Buste', '<span class="couleur1">M Buste</span>', $description);
+        $description = str_replace('XL Buste', '<span class="couleur1">XL Buste</span>', $description);
+        $description = str_replace('Couleur', '<span class="couleur1">Couleur</span>', $description);
+        $description = str_replace('manche', '<span class="couleur1">manche</span>', $description);
+        $description = str_replace('Type de coupe', '<span class="couleur1">Type de coupe</span>', $description);
+        $description = str_replace('Tissu', '<span class="couleur1">Tissu</span>', $description);
+        $description = str_replace('Matériau', '<span class="couleur1">Matériau</span>', $description);
+        $description = str_replace('Composition', '<span class="couleur1">Composition </span>', $description);
+        $description = str_replace(' Instructions d\'entretien ', '<span class="couleur1">Instructions d\'entretien </span>', $description);
+        $description = str_replace(' Corps', '<span class="couleur1">Corps </span>', $description);
+        $description = str_replace(' Conseils d\'entretien ', '<span class="couleur1">Conseils d\'entretien </span>', $description);
+        $description = str_replace(' Col ', '<span class="couleur1"> Col </span>', $description);
+
+
+
+
+        // echo '<h3>' . $productDetails['descriptionDetaille'] . '</h3>';
+        echo '<h3>' . $description . '</h3>';
+
+        echo '<h2>Prix: ' . $productDetails['prix'] . '€' . '</h2>';
+
         echo '</div>';
     } else {
         echo 'Erreur : Impossible de récupérer les détails du produit.';
@@ -30,3 +65,4 @@ if (isset($_GET['id'])) {
 } else {
     echo 'Erreur : ID du produit manquant dans l\'URL.';
 }
+
